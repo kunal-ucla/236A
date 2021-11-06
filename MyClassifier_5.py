0eab53bdc8b07b39fac62e20481d200a328d4e4b
Kunal Kishore <kunalkishore.mail@gmail.com>
2021-11-06 13:43:02 -0700

    Fix errors

--- MyClassifier_5.py
+++ MyClassifier_5.py
@@ -8,12 +8,16 @@ class ifier:
 
 	def sample_selection(self, training_sample):
 		# select from training_sample[] and append to self.train_set[]
+		return
 
 	def train(self, train_data, train_label):
 		# fill self.w and self.b
+		return
 
 	def f(self, input):
 		# decision function based on g(y)
+		return
 
 	def test(self, test_set):
 		# return vector with classification decision
+		return
