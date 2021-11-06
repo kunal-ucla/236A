5ae5ef57f0cfa53c21c19a5fa36a2363ac7812e7
Kunal Kishore <kunalkishore.mail@gmail.com>
2021-11-06 13:41:32 -0700

    Made the basic code layout

---
+++ MyClassifier_5.py
@@ -0,0 +1,19 @@
+class ifier:
+	def __init__(self, num_classes, num_features):
+		C=num_classes
+		M=num_features
+		train_set=[]
+		w=[]
+		b=[]
+
+	def sample_selection(self, training_sample):
+		# select from training_sample[] and append to self.train_set[]
+
+	def train(self, train_data, train_label):
+		# fill self.w and self.b
+
+	def f(self, input):
+		# decision function based on g(y)
+
+	def test(self, test_set):
+		# return vector with classification decision
