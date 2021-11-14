74d4b53030b0966de6d999bae9f5ae2bb3f370d0
Kunal Kishore <kunalkishore.mail@gmail.com>
2021-11-14 04:44:41 -0800

    Spaces instead of tabs

--- MyClassifier_5.py
+++ MyClassifier_5.py
@@ -2,144 +2,137 @@ import numpy as np
 import random
 
 class ifier:
-	def __init__(self, class1, class2, num_features, alpha, lamda, epochs, method, shuffle, prob, init, delta, limit):
-		self.c=np.array([class1,class2])
-		self.M=num_features
-		self.train_set=np.empty((0,num_features))
-		self.train_label=np.array([])
-		self.w=np.zeros(self.M)
-		self.b=0
-		self.alpha=alpha
-		self.lamda=lamda
-		self.epochs=epochs
-		self.method=method
-		self.shuffle=shuffle
-		self.prob=prob
-		self.init=init
-		self.delta=delta
+    def __init__(self, class1, class2, num_features, alpha, lamda, epochs, method, shuffle, prob, init, delta, limit):
+        self.c=np.array([class1,class2])
+        self.M=num_features
+        self.train_set=np.empty((0,num_features))
+        self.train_label=np.array([])
+        self.w=np.zeros(self.M)
+        self.b=0
+        self.alpha=alpha
+        self.lamda=lamda
+        self.epochs=epochs
+        self.method=method
+        self.shuffle=shuffle
+        self.prob=prob
+        self.init=init
+        self.delta=delta
 
-		# test
-		self.counter=0
-		self.limit=limit
+        # test
+        self.counter=0
+        self.limit=limit
 
-	def sample_selection(self, training_sample, training_label):
-		'''
-		Input:
-		training_sample		:	numpy array of shape (M,)	:	current sample
-		training_label		:	int							:	current label
+    def sample_selection(self, training_sample, training_label):
+        '''
+        Input:
+        training_sample     :    numpy array of shape (M,)    :    current sample
+        training_label      :    int                          :    current label
 
-		Output:
-		self.train_set[]	:	numpy array of shape (N,M)	:	append current_sample to this if selected
-		self.train_label[]	:	numpy array of shape (N,)	:	append current_label to this if selected
-		'''
+        Output:
+        self.train_set[]    :    numpy array of shape (N,M)   :    append current_sample to this if selected
+        self.train_label[]  :    numpy array of shape (N,)    :    append current_label to this if selected
+        '''
 
-		# code here for checking whether curr_sample should be included
-		if self.prob==0:
-			if np.shape(self.train_set)[0] < self.init:
-				is_selected=1 # select the 1st 100(self.init) samples for sure
-				self.counter=self.limit
-			else:
-				if self.counter==self.limit:
-					self.train(self.train_set, self.train_label)
-					self.counter=0
-				else:
-					self.counter+=1
-				x=np.append(training_sample,1)
-				w=np.append(self.w,self.b)
-				y=1 if training_label==self.c[0] else -1
-				p=np.dot(x,w)
+        # code here for checking whether curr_sample should be included
+        if self.prob==0:
+            if np.shape(self.train_set)[0] < self.init:
+                is_selected=1 # select the 1st 100(self.init) samples for sure
+                self.counter=self.limit
+            else:
+                if self.counter==self.limit:
+                    self.train(self.train_set, self.train_label)
+                    self.counter=0
+                else:
+                    self.counter+=1
+                x=np.append(training_sample,1)
+                w=np.append(self.w,self.b)
+                y=1 if training_label==self.c[0] else -1
+                p=np.dot(x,w)
 
-				if abs(p) < self.delta:
-					is_selected=1
-				else:
-					is_selected=0
-		else:
-			is_selected=np.random.choice([0,1],p=[1-self.prob,self.prob])
+                if abs(p) < self.delta:
+                    is_selected=1
+                else:
+                    is_selected=0
+        else:
+            is_selected=np.random.choice([0,1],p=[1-self.prob,self.prob])
 
-		if is_selected:
-			self.train_set=np.append(self.train_set, np.reshape(training_sample,(1,len(training_sample))), axis=0)
-			self.train_label=np.append(self.train_label, training_label)
-		
-		return is_selected
+        if is_selected:
+            self.train_set=np.append(self.train_set, np.reshape(training_sample,(1,len(training_sample))), axis=0)
+            self.train_label=np.append(self.train_label, training_label)
+        
+        return is_selected
 
-	def train(self, train_data, train_label):
-		'''
-		Input:
-		train_data	:	numpy array of shape (N,M)	:	features
-		train_label	:	numpy array of shape (N,)	:	labels
-		self.alpha	:	float						:	learning rate
-		self.lamda	:	float						:	regularization param
-		self.epochs	:	int							:	number of epochs
+    def train(self, train_data, train_label):
+        '''
+        Input:
+        train_data    :    numpy array of shape (N,M)   :    features
+        train_label   :    numpy array of shape (N,)    :    labels
+        self.alpha    :    float                        :    learning rate
+        self.lamda    :    float                        :    regularization param
+        self.epochs   :    int                          :    number of epochs
 
-		Output:
-		self.w		:	numpy array of shape (M,)	:	weights
-		self.b		:	float						:	bias term
-		'''
+        Output:
+        self.w        :    numpy array of shape (M,)    :    weights
+        self.b        :    float                        :    bias term
+        '''
 
-		def grad_func(w,x,y):
-			'''
-			Input:
-			w,x,y
+        def grad_func(w,x,y):
+            # calculates gradient
+            # if only 1 example is passed (SGD)
+            if type(y) == np.float64:
+                y = np.array([y])
+                x = np.array([x])
 
-			Output:
-			gradient
-			'''
-			
-			# if only 1 example is passed (SGD)
-			if type(y) == np.float64:
-				y = np.array([y])
-				x = np.array([x])
+            diff=1-y*(np.dot(x,w))
+            grad=np.zeros(len(w))
+            for i,d in enumerate(diff):
+                if max(0,d)!=0:
+                    dw=2*w*self.lamda-y[i]*x[i]
+                else:
+                    dw=2*w*self.lamda
+                grad=grad+dw
+            grad=grad/np.shape(diff)[0]
+            return grad
 
-			diff=1-y*(np.dot(x,w))
-			grad=np.zeros(len(w))
-			for i,d in enumerate(diff):
-				if max(0,d)!=0:
-					dw=2*w*self.lamda-y[i]*x[i]
-				else:
-					dw=2*w*self.lamda
-				grad=grad+dw
-			grad=grad/np.shape(diff)[0]
-			return grad
+        y=np.zeros(len(train_label))
+        y[train_label==self.c[0]]=1
+        y[train_label==self.c[1]]=-1
+        w=np.append(self.w,self.b)
+        x=np.append(train_data,np.ones([np.shape(train_data)[0],1]),axis=1)
 
-		y=np.zeros(len(train_label))
-		y[train_label==self.c[0]]=1
-		y[train_label==self.c[1]]=-1
-		w=np.append(self.w,self.b)
-		x=np.append(train_data,np.ones([np.shape(train_data)[0],1]),axis=1)
+        for e in range(0, self.epochs):
+            if self.method=='sgd':
+                shuffle=list(range(0,np.shape(y)[0]))
+                if self.shuffle:
+                    random.shuffle(shuffle) # shuffle at every epoch
+                for i in shuffle:
+                    gradient=grad_func(w,x[i,:],y[i])
+                    w=w-self.alpha*gradient
+            elif self.method=='gd':
+                gradient=grad_func(w,x,y)
+                w=w-self.alpha*gradient
+            elif self.method=='bgd':
+                end=np.shape(y)[0]
+                start=max(0,end-20) # use last 20 samples at every step
+                gradient=grad_func(w,x[start:end,:],y[start:end])
+                w=w-self.alpha*gradient
+            else:
+                print("Invalid method, try again")
 
-		for e in range(0, self.epochs):
-			if self.method=='sgd':
-				shuffle=list(range(0,np.shape(y)[0]))
-				if self.shuffle:
-					random.shuffle(shuffle) # shuffle at every epoch
-				for i in shuffle:
-					gradient=grad_func(w,x[i,:],y[i])
-					w=w-self.alpha*gradient
-			elif self.method=='gd':
-				gradient=grad_func(w,x,y)
-				w=w-self.alpha*gradient
-			elif self.method=='bgd':
-				end=np.shape(y)[0]
-				start=max(0,end-20) # use last 20 samples at every step
-				gradient=grad_func(w,x[start:end,:],y[start:end])
-				w=w-self.alpha*gradient
-			else:
-				print("Invalid method, try again")
+        self.w=w[0:-1]
+        self.b=w[-1]
 
-		self.w=w[0:-1]
-		self.b=w[-1]
+    def f(self, input):
+        # decision function based on g(y)
+        dec=np.zeros(len(input))
+        for index,value in enumerate(input):
+            if value>=0:
+                dec[index]=self.c[0]
+            else:
+                dec[index]=self.c[1]
+        return dec
 
-	def f(self, input):
-		# decision function based on g(y)
-		dec=np.zeros(len(input))
-		for index,value in enumerate(input):
-			if value>=0:
-				dec[index]=self.c[0]
-			else:
-				dec[index]=self.c[1]
-		return dec
-
-	def test(self, test_set):
-		# return vector with classification decision
-		g=np.dot(test_set,self.w)+self.b
-		return self.f(g)
+    def test(self, test_set):
+        # return vector with classification decision
+        g=np.dot(test_set,self.w)+self.b
+        return self.f(g)
--- test1.py
+++ test1.py
@@ -8,184 +8,184 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 class tester:
-	def __init__(self, syn=0):
-		self.loaded = 0
-		self.syn = syn
-		self.size = 500
+    def __init__(self, syn=0):
+        self.loaded = 0
+        self.syn = syn
+        self.size = 500
 
-	def load_train(self, filename):
-		if self.syn == 1:
-			x=np.transpose(normal(loc=[-1,1],scale=1,size=[4*self.size ,2]))	# class 1
-			y=np.transpose(normal(loc=[1,-1],scale=1,size=[4*self.size ,2]))	# class -1
-			choice = np.random.randint(2, size = np.shape(x)[1]).astype(bool)
-			self.train_data_all = np.transpose(np.where(choice, x, y))
-			self.train_label_all = np.where(choice, 1, -1)
-		else:
-			with open(filename, "r") as fid:
-				data = np.loadtxt(fid, delimiter=",")
-			self.train_data_all = data[:,1:]
-			self.train_label_all = data[:,0]
-		self.loaded = min(2,1+self.loaded)
+    def load_train(self, filename):
+        if self.syn == 1:
+            x=np.transpose(normal(loc=[-1,1],scale=1,size=[4*self.size ,2]))    # class 1
+            y=np.transpose(normal(loc=[1,-1],scale=1,size=[4*self.size ,2]))    # class -1
+            choice = np.random.randint(2, size = np.shape(x)[1]).astype(bool)
+            self.train_data_all = np.transpose(np.where(choice, x, y))
+            self.train_label_all = np.where(choice, 1, -1)
+        else:
+            with open(filename, "r") as fid:
+                data = np.loadtxt(fid, delimiter=",")
+            self.train_data_all = data[:,1:]
+            self.train_label_all = data[:,0]
+        self.loaded = min(2,1+self.loaded)
 
-	def load_test(self, filename):
-		if self.syn == 1:
-			x=np.transpose(normal(loc=[-1,1],scale=1,size=[self.size ,2]))	# class 1
-			y=np.transpose(normal(loc=[1,-1],scale=1,size=[self.size ,2]))	# class -1
-			choice = np.random.randint(2, size = np.shape(x)[1]).astype(bool)
-			self.test_data_all = np.transpose(np.where(choice, x, y))
-			self.test_label_all = np.where(choice, 1, -1)
-		else:
-			with open(filename, "r") as fid:
-				data = np.loadtxt(fid, delimiter=",")
-			self.test_data_all = data[:,1:]
-			self.test_label_all = data[:,0]
-		self.loaded = min(2,1+self.loaded)
+    def load_test(self, filename):
+        if self.syn == 1:
+            x=np.transpose(normal(loc=[-1,1],scale=1,size=[self.size ,2]))    # class 1
+            y=np.transpose(normal(loc=[1,-1],scale=1,size=[self.size ,2]))    # class -1
+            choice = np.random.randint(2, size = np.shape(x)[1]).astype(bool)
+            self.test_data_all = np.transpose(np.where(choice, x, y))
+            self.test_label_all = np.where(choice, 1, -1)
+        else:
+            with open(filename, "r") as fid:
+                data = np.loadtxt(fid, delimiter=",")
+            self.test_data_all = data[:,1:]
+            self.test_label_all = data[:,0]
+        self.loaded = min(2,1+self.loaded)
 
-	def select_class(self, choice1, choice2):
-		if self.syn == 1:
-			self.choice1 = 1
-			self.choice2 = -1
-			self.train_data = self.train_data_all
-			self.train_label = self.train_label_all
-			self.test_data = self.test_data_all
-			self.test_label = self.test_label_all
-		else:
-			self.choice1 = choice1
-			self.choice2 = choice2
+    def select_class(self, choice1, choice2):
+        if self.syn == 1:
+            self.choice1 = 1
+            self.choice2 = -1
+            self.train_data = self.train_data_all
+            self.train_label = self.train_label_all
+            self.test_data = self.test_data_all
+            self.test_label = self.test_label_all
+        else:
+            self.choice1 = choice1
+            self.choice2 = choice2
 
-			select = (self.train_label_all==choice1) | (self.train_label_all==choice2)
-			self.train_data = self.train_data_all[select,:]
-			self.train_label = self.train_label_all[select]
+            select = (self.train_label_all==choice1) | (self.train_label_all==choice2)
+            self.train_data = self.train_data_all[select,:]
+            self.train_label = self.train_label_all[select]
 
-			select = (self.test_label_all==choice1) | (self.test_label_all==choice2)
-			self.test_data = self.test_data_all[select,:]
-			self.test_label = self.test_label_all[select]
+            select = (self.test_label_all==choice1) | (self.test_label_all==choice2)
+            self.test_data = self.test_data_all[select,:]
+            self.test_label = self.test_label_all[select]
 
-	def train(self, normal, alpha, lamda, epochs, method, select, shuffle, prob, init, delta, limit):
-		if normal:
-			scaler = preprocessing.StandardScaler().fit(self.train_data)
-			self.train_data = scaler.transform(self.train_data)
-			self.test_data = scaler.transform(self.test_data)
+    def train(self, normal, alpha, lamda, epochs, method, select, shuffle, prob, init, delta, limit):
+        if normal:
+            scaler = preprocessing.StandardScaler().fit(self.train_data)
+            self.train_data = scaler.transform(self.train_data)
+            self.test_data = scaler.transform(self.test_data)
 
-		self.num_features = np.shape(self.train_data)[1]
-		self.t = classifier(self.choice1,self.choice2, self.num_features, alpha=alpha, lamda=lamda, epochs=epochs, method=method, shuffle=shuffle, prob=prob, init=init, delta=delta, limit=limit)
-		
-		start = time.time()
-		if select:
-			print("Selecting samples...")
-			chosen = 0
-			total = 0
-			for curr_sample,curr_label in zip(self.train_data,self.train_label):
-				chosen = chosen + self.t.sample_selection(curr_sample,curr_label)
-				total = total + 1
-				print('Selected %d out of %d samples' %(chosen,total), end='\r')
-			self.selected_data = self.t.train_set
-			self.selected_label = self.t.train_label
-		else:
-			chosen = np.shape(self.train_data)[0]
-			self.selected_data = self.train_data
-			self.selected_label = self.train_label
-		end = time.time()
-		print("Time taken for selection = %f\n" %(end-start))
-		print('Sending %d samples for training...' %(chosen))
+        self.num_features = np.shape(self.train_data)[1]
+        self.t = classifier(self.choice1,self.choice2, self.num_features, alpha=alpha, lamda=lamda, epochs=epochs, method=method, shuffle=shuffle, prob=prob, init=init, delta=delta, limit=limit)
+        
+        start = time.time()
+        if select:
+            print("Selecting samples...")
+            chosen = 0
+            total = 0
+            for curr_sample,curr_label in zip(self.train_data,self.train_label):
+                chosen = chosen + self.t.sample_selection(curr_sample,curr_label)
+                total = total + 1
+                print('Selected %d out of %d samples' %(chosen,total), end='\r')
+            self.selected_data = self.t.train_set
+            self.selected_label = self.t.train_label
+        else:
+            chosen = np.shape(self.train_data)[0]
+            self.selected_data = self.train_data
+            self.selected_label = self.train_label
+        end = time.time()
+        print("Time taken for selection = %f\n" %(end-start))
+        print('Sending %d samples for training...' %(chosen))
 
-		if (select==1):
-			self.t.method = 'gd' # use GD once and for all after selection
-			self.t.epochs = 70
-		
-		start = time.time()
-		self.t.train(self.selected_data,self.selected_label)
-		end = time.time()
-		print("Time taken for training = %f\n" %(end-start))
+        if (select==1):
+            self.t.method = 'gd' # use GD once and for all after selection
+            self.t.epochs = 70
+        
+        start = time.time()
+        self.t.train(self.selected_data,self.selected_label)
+        end = time.time()
+        print("Time taken for training = %f\n" %(end-start))
 
-	def test(self):
-		print("Starting tests...")
+    def test(self):
+        print("Starting tests...")
 
-		train_pred = self.t.test(self.train_data)
-		error_count = np.count_nonzero((train_pred-self.train_label)!=0)
-		total_count = len(train_pred)
-		error_perc = error_count*100/total_count
-		print("Train Error percentage =",error_perc)
+        train_pred = self.t.test(self.train_data)
+        error_count = np.count_nonzero((train_pred-self.train_label)!=0)
+        total_count = len(train_pred)
+        error_perc = error_count*100/total_count
+        print("Train Error percentage =",error_perc)
 
-		test_pred = self.t.test(self.test_data)
-		error_count = np.count_nonzero((test_pred-self.test_label)!=0)
-		total_count = len(test_pred)
-		error_perc = error_count*100/total_count
-		print("Test Error percentage =",error_perc)
+        test_pred = self.t.test(self.test_data)
+        error_count = np.count_nonzero((test_pred-self.test_label)!=0)
+        total_count = len(test_pred)
+        error_perc = error_count*100/total_count
+        print("Test Error percentage =",error_perc)
 
-	def plot(self):
-		colors=['red','blue']
-		y=self.selected_label
-		y[y==-1]=0
-		plt.figure(1)
-		plt.scatter(self.selected_data[:,0],self.selected_data[:,1],c=y,cmap=matplotlib.colors.ListedColormap(colors))
+    def plot(self):
+        colors=['red','blue']
+        y=self.selected_label
+        y[y==-1]=0
+        plt.figure(1)
+        plt.scatter(self.selected_data[:,0],self.selected_data[:,1],c=y,cmap=matplotlib.colors.ListedColormap(colors))
 
-		colors=['red','blue']
-		y=self.train_label
-		y[y==-1]=0
-		plt.figure(2)
-		plt.scatter(self.train_data[:,0],self.train_data[:,1],c=y,cmap=matplotlib.colors.ListedColormap(colors))
-		
-		plt.show()
+        colors=['red','blue']
+        y=self.train_label
+        y[y==-1]=0
+        plt.figure(2)
+        plt.scatter(self.train_data[:,0],self.train_data[:,1],c=y,cmap=matplotlib.colors.ListedColormap(colors))
+        
+        plt.show()
 
-	def run(self, **kwargs):
-		if "train_file" not in kwargs:
-			kwargs["train_file"]='mnist_train.csv'
-		else:
-			self.loaded=0
-		if "test_file" not in kwargs:
-			kwargs["test_file"]='mnist_test.csv'
-		else:
-			self.loaded=0
-		if "class1" not in kwargs:
-			kwargs["class1"]=1
-		if "class2" not in kwargs:
-			kwargs["class2"]=7
-		if "normal" not in kwargs:
-			kwargs["normal"]=0
-		if "alpha" not in kwargs:
-			kwargs["alpha"]=0.1
-		if "lamda" not in kwargs:
-			kwargs["lamda"]=0.1
-		if "epochs" not in kwargs:
-			kwargs["epochs"]=50
-		if "method" not in kwargs:
-			kwargs["method"]='gd'
-		if "select" not in kwargs:
-			kwargs["select"]=0
-		if "shuffle" not in kwargs:
-			kwargs["shuffle"]=0
-		if "prob" not in kwargs:
-			kwargs["prob"]=0.2
-		if "init" not in kwargs:
-			kwargs["init"]=10
-		if "delta" not in kwargs:
-			kwargs["delta"]=1
-		if "limit" not in kwargs:
-			kwargs["limit"]=0
+    def run(self, **kwargs):
+        if "train_file" not in kwargs:
+            kwargs["train_file"]='mnist_train.csv'
+        else:
+            self.loaded=0
+        if "test_file" not in kwargs:
+            kwargs["test_file"]='mnist_test.csv'
+        else:
+            self.loaded=0
+        if "class1" not in kwargs:
+            kwargs["class1"]=1
+        if "class2" not in kwargs:
+            kwargs["class2"]=7
+        if "normal" not in kwargs:
+            kwargs["normal"]=0
+        if "alpha" not in kwargs:
+            kwargs["alpha"]=0.1
+        if "lamda" not in kwargs:
+            kwargs["lamda"]=0.1
+        if "epochs" not in kwargs:
+            kwargs["epochs"]=50
+        if "method" not in kwargs:
+            kwargs["method"]='gd'
+        if "select" not in kwargs:
+            kwargs["select"]=0
+        if "shuffle" not in kwargs:
+            kwargs["shuffle"]=0
+        if "prob" not in kwargs:
+            kwargs["prob"]=0.2
+        if "init" not in kwargs:
+            kwargs["init"]=10
+        if "delta" not in kwargs:
+            kwargs["delta"]=1
+        if "limit" not in kwargs:
+            kwargs["limit"]=0
 
 
-		if self.loaded != 2:
-			self.load_train(filename=kwargs["train_file"])
-			self.load_test(filename=kwargs["test_file"])
-			self.loaded = 2
+        if self.loaded != 2:
+            self.load_train(filename=kwargs["train_file"])
+            self.load_test(filename=kwargs["test_file"])
+            self.loaded = 2
 
-		self.select_class(choice1=kwargs["class1"], choice2=kwargs["class2"])
-		self.train(normal=kwargs["normal"], alpha=kwargs["alpha"], lamda=kwargs["lamda"], epochs=kwargs["epochs"], method=kwargs["method"], select=kwargs["select"], shuffle=kwargs["shuffle"], prob=kwargs["prob"], init=kwargs["init"], delta=kwargs["delta"], limit=kwargs["limit"])
-		self.test()
+        self.select_class(choice1=kwargs["class1"], choice2=kwargs["class2"])
+        self.train(normal=kwargs["normal"], alpha=kwargs["alpha"], lamda=kwargs["lamda"], epochs=kwargs["epochs"], method=kwargs["method"], select=kwargs["select"], shuffle=kwargs["shuffle"], prob=kwargs["prob"], init=kwargs["init"], delta=kwargs["delta"], limit=kwargs["limit"])
+        self.test()
 
 if len(sys.argv)>1:
-	if sys.argv[1]=='-s':
-		test = tester(1)
-		if len(sys.argv)>2:
-			test.size = int(sys.argv[2])
-		print("Loading synthetic datasets...")
-	else:
-		test = tester()
-		print("Loading default datasets...")
+    if sys.argv[1]=='-s':
+        test = tester(1)
+        if len(sys.argv)>2:
+            test.size = int(sys.argv[2])
+        print("Loading synthetic datasets...")
+    else:
+        test = tester()
+        print("Loading default datasets...")
 else:
-	test = tester()
-	print("Loading default datasets...")
+    test = tester()
+    print("Loading default datasets...")
 test.load_train('mnist_train.csv')
 test.load_test('mnist_test.csv')
 print("Ready. Start by running 'test.run()'")
